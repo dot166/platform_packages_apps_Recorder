2cd481a883147452928668dc64af851ea008f892
._______166 <retromac9x2@gmail.com>
2025-02-24 23:34:11 +0000

    cleanup RecorderApplication.kt

--- app/src/main/java/org/lineageos/recorder/RecorderApplication.kt
+++ app/src/main/java/org/lineageos/recorder/RecorderApplication.kt
@@ -5,7 +5,6 @@
 
 package org.lineageos.recorder
 
-import com.google.android.material.color.DynamicColors
 import io.github.dot166.jLib.ThemeEngine.ThemeEngine.setCustomThemeClass
 import io.github.dot166.jLib.app.jLIBCoreApp
 import io.github.dot166.recorder.themes
@@ -14,8 +13,5 @@ class RecorderApplication : jLIBCoreApp() {
     override fun onCreate() {
         super.onCreate()
         setCustomThemeClass(themes())
-
-        // Observe dynamic colors changes
-        //DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
