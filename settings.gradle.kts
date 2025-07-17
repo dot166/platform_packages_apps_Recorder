73aa838ba351469ed2b9bcf8848c60f2aec78b10
._______166 <retromac9x2@gmail.com>
2025-07-18 00:48:21 +0100

    update jLib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven("https://raw.githubusercontent.com/dot166/gradle-generatebp/v1.21.1/.m2")
+        maven("https://raw.githubusercontent.com/dot166/gradle-generatebp/v1.25.1/.m2")
     }
 }
 
